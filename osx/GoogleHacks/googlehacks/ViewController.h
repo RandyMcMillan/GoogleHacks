772bbe06b0f621945a6865737cb625edcaf5e76c
Randy McMillan <randy.lee.mcmillan@gmail.com>
2015-07-12 02:00:54 -0400

    work on type_str more...

--- osx/GoogleHacks/googlehacks/ViewController.h
+++ osx/GoogleHacks/googlehacks/ViewController.h
@@ -16,7 +16,7 @@
 
 - (IBAction)getSearchField:(id)sender;
 - (NSString *)returnSearchField;
-- (NSURL *)modifiedURL;
+- (NSURL *)modifiedURL:(NSString *)modString;
 - (void)openURL:(NSURL *)url inBackground:(BOOL)background;
 - (IBAction)openURLFromButton:(id)sender;
 - (IBAction)audioExtension:(id)sender;
--- osx/GoogleHacks/googlehacks/ViewController.m
+++ osx/GoogleHacks/googlehacks/ViewController.m
@@ -27,19 +27,6 @@
 	[self.data_object = [DataObjects alloc] init];
 	[self.data_object loadValues:nil];
 
-	// Search String
-	// self.data_object.search_str = @"Search String";
-	// self.searchField. = self.data_object.search_str;
-	// [self.searchTextField setStringValue:self.data_object.search_str];
-	// self.data_object.full_search_str = @"Full Search String";
-	//   self.data_object.types_str = @"";
-
-#ifndef DEBUG
-		// NSLog(self.data_object.search_str);
-		// NSLog(self.data_object.full_search_str);
-		// NSLog(self.data_object.types_str);
-#endif
-
 	// images
 	// ????
 
@@ -90,7 +77,6 @@
 
 // full_search_str ="http://www.google.com/search?hl=en&q=-inurl%3A%28htm%7Chtml%7Cphp%29+intitle%3A%22index+of%22+%2B%22last+modified%22+%2B%22parent+directory%22+%2Bdescription+%2Bsize+%2B%28" +types+ "%29+%22"+search_str+"%22"
 
-
 - (void)getSearchField:(id)sender
 {
 	// http://stackoverflow.com/questions/16704156/how-to-open-external-links-to-safari-chrome-browser-in-cocoa
@@ -98,75 +84,50 @@
 	// TODO handle white spaces
 	// http://stackoverflow.com/questions/7628470/remove-all-whitespaces-from-nsstring
 	// [self.data_object.full_search_str  stringByReplacingOccurrencesOfString:@" " withString:@"+"];
-    // http://stackoverflow.com/questions/3439853/replace-occurances-of-space-in-url
-    // strip spaces from user before direct manipulation
+	// http://stackoverflow.com/questions/3439853/replace-occurances-of-space-in-url
+	// strip spaces from user before direct manipulation
 
+	NSURL *url = [self modifiedURL:nil];
 
-    NSURL *url = [self modifiedURL:nil];
-    NSLog(@"url = %@",url);
+	NSLog(@"url = %@", url);
 	NSString *scheme = [url scheme];
 
 	if ([scheme isEqualToString:@"file"]) {} else {
-        [self assembleTypesString];
-        NSLog(@"before else... = %@",self.data_object.types_str);
-        NSLog(@"%@",self.data_object.types_str);
+		[self assembleTypesString];
+		NSLog(@"before else... = %@", self.data_object.types_str);
+		NSLog(@"%@", self.data_object.types_str);
+
 		if ([self.data_object.types_str isEqualToString:@""]) {
 			[self openURL:url inBackground:YES];
 		} else {
-            
-            NSLog(@"after else... = %@",self.data_object.types_str);
-
-			// append with types
-
-			// self.data_object.full_search_str ="http://www.google.com/search?hl=en&q=-inurl%3A%28htm%7Chtml%7Cphp%29+intitle%3A%22index+of%22+%2B%22last+modified%22+%2B%22parent+directory%22+%2Bdescription+%2Bsize+%2B%28" +types+ "%29+%22"+search_str+"%22"
-
-			//self.data_object.full_search_str = @"http://www.google.com/search?hl=en&q=-inurl%3A%28htm%7Chtml%7Cphp%29+intitle%3A%22index+of%22+%2B%22last+modified%22+%2B%22parent+directory%22+%2Bdescription+%2Bsize+%2B%28";
-
-			// %2B%28%29%22%2B
-			// https://www.google.com/search?hl=en&q=-inurl%3A(htm%7Chtml%7Cphp)+intitle%3A%22index+of%22+%2B%22last+modified%22+%2B%22parent+directory%22+%2Bdescription+%2Bsize+%2B(.mp3%7C.pdf%7Ctest%22&gws_rd=ssl
-
-			//self.data_object.full_search_str	= [self.data_object.full_search_str stringByAppendingString:self.data_object.types_str];
-			//self.data_object.full_search_str	= [self.data_object.full_search_str stringByAppendingString:@"%29+%22"];
-			//self.data_object.full_search_str	= [self.data_object.full_search_str stringByAppendingString:self.data_object.search_str];
-			//self.data_object.full_search_str	= [self.data_object.full_search_str stringByAppendingString:@"%22"];
-
-			// .mpg%29+%22test%22";
-
-			// open afgter append
+			NSLog(@"after else... = %@", self.data_object.types_str);
 			[self openURL:url inBackground:YES];
 		}
 	}
-
-	// NSLog(@"__________getSearchField_________");
-
 }
 
+- (NSString *)returnSearchField
+{
+	NSString *returnSearchField = nil;
 
-
-- (NSString *)returnSearchField{
-
-    NSString *returnSearchField = nil;
-    returnSearchField = [[self.searchTextField stringValue] stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
-    return returnSearchField;
-
+	returnSearchField = [[self.searchTextField stringValue] stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
+	return returnSearchField;
 }
 
 - (NSURL *)modifiedURL:(NSString *)modString
 {
+	NSLog(@"%@", modString);// null
 
-    NSLog(@"%@",modString);//null
+	// self.data_object.search_str = [self returnSearchField];
+	modString = [BASE_URL stringByAppendingString:@"q="];
+	NSLog(@"%@", modString);
+	modString = [modString stringByAppendingString:[self returnSearchField]];
+	NSLog(@"%@", modString);
 
-    //self.data_object.search_str = [self returnSearchField];
-    modString = [BASE_URL stringByAppendingString:@"q="];
-    NSLog(@"%@",modString);
-    modString = [modString stringByAppendingString:[self returnSearchField]];
-    NSLog(@"%@",modString);
+	// modString = [modString stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
+	// NSLog(@"%@",modString);
 
-    //modString = [modString stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
-    //NSLog(@"%@",modString);
-
-    
-    NSURL *modifiedURL = [NSURL URLWithString:modString];
+	NSURL *modifiedURL = [NSURL URLWithString:modString];
 
 	return modifiedURL;
 }
@@ -228,9 +189,8 @@
 		// NSLog(@"self.data_object.wma_check IS : %@", (self.data_object.wma_check == TRUE) ? @"TRUE" : @"FALSE");
 		// NSLog(@"self.data_object.ogg_check IS : %@", (self.data_object.ogg_check == TRUE) ? @"TRUE" : @"FALSE");
 #endif
-    
-    [self assembleTypesString];
 
+	[self assembleTypesString];
 }
 
 - (IBAction)videoExtension:(id)sender
@@ -292,9 +252,8 @@
 		// NSLog(@"self.data_object.divx_check IS : %@", (self.data_object.divx_check == TRUE) ? @"TRUE" : @"FALSE");
 		// NSLog(@"self.data_object.flv_check IS : %@", (self.data_object.flv_check == TRUE) ? @"TRUE" : @"FALSE");
 #endif
-    
-    [self assembleTypesString];
 
+	[self assembleTypesString];
 }
 
 - (IBAction)documentExtension:(id)sender
@@ -451,9 +410,8 @@
 		// NSLog(@"self.data_object.zip1_check IS : %@", (self.data_object.zip1_check == TRUE) ? @"TRUE" : @"FALSE");
 		// NSLog(@"self.data_object.odt_check IS : %@", (self.data_object.odt_check == TRUE) ? @"TRUE" : @"FALSE");
 #endif
-    
-    [self assembleTypesString];
 
+	[self assembleTypesString];
 }
 
 - (IBAction)passwordQuery:(id)sender
@@ -576,9 +534,8 @@
 		// NSLog(@"self.data_object.pass_5 IS : %@", (self.data_object.pass_5 == TRUE) ? @"TRUE" : @"FALSE");
 		// NSLog(@"self.data_object.pass_6 IS : %@", (self.data_object.pass_6 == TRUE) ? @"TRUE" : @"FALSE");
 #endif
-    
-    [self assembleTypesString];
 
+	[self assembleTypesString];
 }
 
 - (IBAction)fileExtension:(id)sender
@@ -627,9 +584,8 @@
 		// NSLog(@"self.data_object.zip_check IS : %@", (self.data_object.zip_check == TRUE) ? @"TRUE" : @"FALSE");
 		// NSLog(@"self.data_object.ddl_check IS : %@", (self.data_object.ddl_check == TRUE) ? @"TRUE" : @"FALSE");
 #endif
-    
-    [self assembleTypesString];
 
+	[self assembleTypesString];
 }
 
 - (IBAction)linkQuery:(id)sender
@@ -641,9 +597,8 @@
 
 	if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else if ([selCell tag] == 0) {} else {// NSLog(@"Error");
 	}
-    
-    [self assembleTypesString];
 
+	[self assembleTypesString];
 }
 
 - (void)assembleTypesString
@@ -655,135 +610,162 @@
 	if (self.data_object.mp3_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".mp3%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
+
 	if (self.data_object.wma_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".wma%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.ogg_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".ogg%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	// Docs
 	if (self.data_object.pdf_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".pdf%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.txt_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".txt%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.lit_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".lit%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.rar_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".rar%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.doc_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".doc%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.rtf_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".rtf%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pps_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".pps%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.chm_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".chm%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.zip1_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".zip1%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.odt_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".odt%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	// Video
 	if (self.data_object.mpg_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".mpg%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.avi_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".avi%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.wmv_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".wmv%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.divx_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".divx%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.flv_check == TRUE) {
 		self.data_object.types_str = [self.data_object.types_str stringByAppendingString:@".flv%7C"];
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	// Passwords
 
 	if (self.data_object.pass_check == TRUE) {
 		self.data_object.full_search_str = @"";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_text == TRUE) {
 		self.data_object.full_search_str = @"";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_1 == TRUE) {
 		self.data_object.full_search_str = @"http://www.google.com/search?q=intitle%3A%22Index+of%22+passwords+modified";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_2 == TRUE) {
 		self.data_object.full_search_str = @"http://www.google.com/search?hl=en&q=allinurl%3Aauth_user_file.txt";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_3 == TRUE) {
 		self.data_object.full_search_str = @"http://www.google.com/search?q=inurl%3Apasslist.txt&btnG=Search";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_4 == TRUE) {
 		self.data_object.full_search_str = @"http://www.google.com/search?q=%22%23+-FrontPage-%22+inurl%3Aservice.pwd";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_5 == TRUE) {
 		self.data_object.full_search_str = @"http://www.google.com/search?q=intitle%3A%22Index+of%22+config.php";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	if (self.data_object.pass_6 == TRUE) {
 		self.data_object.full_search_str = @"http://www.google.com/search?q=%22http%3A%2F%2F*%3A*%40%22";
 	}
-    NSLog(@"%@",self.data_object.types_str);
+
+	NSLog(@"%@", self.data_object.types_str);
 
 	/*
 	 *   if (self.data_object.@@ == TRUE) {
@@ -797,16 +779,14 @@
 
 	// http://www.tutorialspoint.com/objective_c/objective_c_strings.htm
 
-	if ([self.data_object.types_str length]<=7){
+	if ([self.data_object.types_str length] <= 7) {
+		NSLog(@"less than = 7");
+		// http://borkware.com/quickies/one?topic=NSString
 
-	NSLog(@"less than = 7");
-	// http://borkware.com/quickies/one?topic=NSString
+		self.data_object.types_str = [self truncateString:self.data_object.types_str toCharacterCount:4];
 
-	self.data_object.types_str = [self truncateString:self.data_object.types_str toCharacterCount:4];
-
-	// https://bdpuqvsqmphctrcs.onion.to/data/
-
-	  }
+		// https://bdpuqvsqmphctrcs.onion.to/data/
+	}
 
 	NSLog(@"assembleTypesString");
 	NSLog(@"self.data_object.types_str = %@", self.data_object.types_str);
